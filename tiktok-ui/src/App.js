c44eb2c05d99859246f1238fb4eed9aa5d050b52
VuongQuocBao <vuongquocbaoq8@gmail.com>
2025-05-25 19:40:33 +0700

    Cấu hình Router/Layout cho dự án

--- tiktok-ui/src/App.js
+++ tiktok-ui/src/App.js
@@ -1,9 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import DefaultLayout from './components/Layout/DefaultLayout';
+import { Fragment } from 'react';
+
 function App() {
   return (
-    <div>
-      <h1>Haha</h1>
-      <h1>Haha</h1>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
